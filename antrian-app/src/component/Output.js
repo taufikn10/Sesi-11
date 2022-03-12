03f3eb28a241a535a0850b5a9aa9bcc52c0969cb
Taufik Nurrahman <official.taufik980@gmail.com>
2022-03-12 20:04:10 +0700

    sesi 11

--- antrian-app/src/component/Output.js
+++ antrian-app/src/component/Output.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 export default function Output() {
+  const [antrian, setUsername] = useState("[]");
+  const [disable, setDisable] = useState(true);
+
+  useEffect(() => {
+    console.log(disable);
+  }),
+    [antrian, disable];
+
   return (
     <div className="output">
       <p>Antrian Kosong</p>
